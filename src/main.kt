34426e9255dea1bfbf6339bff7bf46c594d00927
JaneMusimbi <janemusimbi@gmail.com>
2022-04-06 17:19:57 +0300

    Assignment7 commit

---
+++ src/main.kt
@@ -0,0 +1,49 @@
+fun main(){
+    numbers()
+    var namz=names(arrayOf("jane","musimbi","speria","alinda") )
+    println(namz)
+    robot(1)
+    multiples()
+
+
+}
+fun numbers(){
+    for(number in 1..100){
+        if((number%2) != 0){
+            println(number)
+        }
+
+    }
+}
+fun names(name:Array<String>):Int{
+    var nam=0
+    name.forEach { x ->
+        if(x.length>5) {
+            nam++
+        }
+    }
+    return nam
+}
+fun robot(guest:Int){
+    if(guest<6){
+        println("milk")
+    }else if(guest<15 && guest>6 ){
+        println("fanta orange")
+    }else{
+        println("cocacola")
+    }
+
+}
+fun multiples(){
+    for(nums in 1..100){
+    if((nums%3)==0 && (nums%5)==0){
+        println("FizzBuzz")
+    }else if((nums%3)==0){
+        println("Fizz")
+    }else if((nums%5)==0){
+        println("Buzz")
+    }else{
+        println(nums)
+    }
+}
+     }
